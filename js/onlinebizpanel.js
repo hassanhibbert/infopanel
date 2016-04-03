1027ce69e129199f7e06a0135e00b70e443d2408
Hassan Hibbert <hassanhibbert@gmail.com>
2016-04-03 19:24:41 -0400

    made updates for building full details.

--- js/onlinebizpanel.js
+++ js/onlinebizpanel.js
@@ -1,36 +1,19 @@
-/* global $, jQuery, document */
+/* global $, jQuery, document, ajaxManager */
 
-var onlineBizPanel = (function ($, global, document) {
+var onlineBizPanel = (function ($, document) {
     var
     // DOM references
         $listContainter,
         $listInnerContainter,
         $toggleElement,
+        $fullDetails,
 
         // module API
-        publicAPI = {},
-
-        transitionEvent = whichTransitionEvent();
+        publicAPI = {};
 
     //###############//
     //### Private ###//
     //###############//
-
-    function whichTransitionEvent() {
-        var t,
-            el = document.createElement('fakeelement'),
-            transitions = {
-                'transition': 'transitionend',
-                'OTransition': 'oTransitionEnd',
-                'MozTransition': 'transitionend',
-                'WebkitTransition': 'webkitTransitionEnd'
-            };
-        for (t in transitions) {
-            if (el.style[t] !== undefined) {
-                return transitions[t];
-            }
-        }
-    }
 
     /**
      * Handler for panel clicks
@@ -39,10 +22,11 @@ var onlineBizPanel = (function ($, global, document) {
      */
     function panelHandler(evt) {
         var $status = $toggleElement.attr('data-status');
+        $('.bottom-wrap, .top-wrap').remove();      
         if ($status === 'accordion') {
             togglePanel();
         } else if ($status === 'back') {
-            //backToFullList();
+            changeContent($listInnerContainter);
         }
     }
 
@@ -50,35 +34,78 @@ var onlineBizPanel = (function ($, global, document) {
         evt.preventDefault();
         var $currentName = $(this).attr('data-businessname'),
             detailsObj = ajaxManager.search('businessName', $currentName);
-        populateFullDetails(detailsObj);
-        //viewFullDetails();
+        buildFullDetails(detailsObj, changeContent.bind(null, $fullDetails));
     }
 
-    function backToFullList() {
-        $toggleElement.attr('data-status', 'accordion').find('h2').text('Online Businesses');
+    function setNewVariables() {
+        var $hideElement = ($element.selector === '#ob-full-details') ? $listInnerContainter : $fullDetails,
+            status = ($element.selector === '#ob-full-details') ? 'back' : 'accordion',
+            toggleText = ($element.selector === '#ob-full-details') ? 'Back' : 'Online Businesses',
+            $currentElement = ($element.selector === '#ob-full-details') ? $element : $listInnerContainter;
+    }
+    function changeContent($element) {
+        setNewVariables()
+            
+        $toggleElement.attr('data-status', status).find('h2').text(toggleText);
+        $hideElement.addClass('ob-hide');
+        $hideElement.toggleClass('ob-fadeOut ob-fadeIn');
+        $element.removeClass('ob-hide');
+        $element.outerHeight();
+        $element.toggleClass('ob-fadeOut ob-fadeIn');
     }
 
-    function viewFullDetails() {
-        $toggleElement.attr('data-status', 'back').find('h2').text('Back');
-    }
-
-    function animationEndHandler(evt) {
-        console.log(evt);
-    }
-
-    function populateFullDetails(data) {
-        var $logo = $('.top-wrap img'),
-            $businessName = $('.top-wrap h2'),
-            $phone = $('.top-wrap a'),
-            $details = $('.bottom-wrap p'),
-            $web = $('.bottom-wrap a'),
-            $mainImg = $('.bottom-wrap a');
-        $logo.attr('src', data.logo);
-        $businessName.text(data.businessName);
-        $phone.text(data.phone);
-        $details.text(data.details);
-        $web.attr('href', data.website).text(data.website);
-        $mainImg.attr('src', data.mainimage);
+    function buildFullDetails(data, cb) {
+        var imgElement,
+        
+            // document fragment
+            docFrag = document.createDocumentFragment(),
+            
+            // create elements
+            topDiv = myCreateElement('div', {class: 'top-wrap clearfix'}),
+            bottomDiv = myCreateElement('div', {class: 'bottom-wrap'}), 
+            hTag = myCreateElement('h2'),
+            aTagPhone = myCreateElement('a', {class: 'ob-phone', href: 'tel:' + data.phone}),
+            aTagWeb = myCreateElement('a', {href: data.website, target: '_blank'}),
+            pTagDetails = myCreateElement('p'),
+            hr = myCreateElement('hr'),
+            
+            // check if images exist
+            logoImg = (data.logo) ? myCreateElement('img', {src: data.logo, alt: 'business logo'}) : undefined,
+            mainImg = (data.mainimage) ? myCreateElement('img', {src: data.mainimage, alt: 'main image'}) : undefined;
+        
+        // append logo if it  exist
+        if (logoImg) {
+            $(topDiv).append(logoImg, hTag, aTagPhone);
+        } else {
+            $(topDiv).append(hTag, aTagPhone);
+        }
+        
+        // append div to document fragment
+        $(hTag).append(data.businessName);
+        $(aTagPhone).append(data.phone);
+        $(docFrag).append(topDiv);
+        
+        // append main image if it exist
+        if (mainImg) {
+            $(bottomDiv).append(pTagDetails, aTagWeb, hr, mainImg);
+        } else {
+            $(bottomDiv).append(pTagDetails, aTagWeb);
+        }
+        
+        // append div to document fragment
+        $(pTagDetails).append(data.details);
+        $(aTagWeb).append(data.website);
+        $(docFrag).append(bottomDiv);
+        
+        // append details data to DOM
+        $fullDetails.append(docFrag);
+        
+        // check to see if image exsist and assign to imgElement
+        imgElement = (logoImg) ? logoImg : mainImg;
+        
+        // if image exist then wait for it to load before executing callback
+        // else execute callback without waiting
+        (imgElement) ? $(imgElement).load(cb) : cb();
     }
 
     /**
@@ -120,19 +147,10 @@ var onlineBizPanel = (function ($, global, document) {
         data.forEach(function (dataObj) {
             // create elements
             var liTag = myCreateElement('li'),
-                linkDetails = myCreateElement('a', {
-                    class: 'ob-details',
-                    href: '#',
-                    'data-businessname': dataObj.businessName
-                }),
+                linkDetails = myCreateElement('a', {class: 'ob-details', href: '#', 'data-businessname': dataObj.businessName}),
                 details = myCreateElement('p'),
-                phone = myCreateElement('div', {
-                    class: 'ob-phone'
-                }),
-                webLink = myCreateElement('a', {
-                    href: dataObj.website,
-                    target: '_blank'
-                }),
+                phone = myCreateElement('div', {class: 'ob-phone'}),
+                webLink = myCreateElement('a', {href: dataObj.website,target: '_blank'}),
                 hTag = myCreateElement('h2');
 
             // append data to elements
@@ -183,6 +201,7 @@ var onlineBizPanel = (function ($, global, document) {
         $listContainter = $(opt.listContainer);
         $listInnerContainter = $(opt.listInnerContainer);
         $toggleElement = $(opt.toggleElement);
+        $fullDetails = $(opt.fullDetails);
 
         // get data
         ajaxManager.loadData(buildList);
@@ -193,21 +212,17 @@ var onlineBizPanel = (function ($, global, document) {
         // listener for event delegation
         $listInnerContainter.on('click', 'a[class^="ob-details"]', detailsHandler);
 
-        if (transitionEvent) {
-            $listInnerContainter.bind(transitionEvent, animationEndHandler);
-        }
     };
 
     return publicAPI;
-})(jQuery, window, document);
+})(jQuery, document);
 
 
 $(document).ready(function () {
     onlineBizPanel.init({
         listContainer: '#online-list-containter',
         listInnerContainer: '#online-list-containter ul',
-        toggleElement: '#online-biz-toggle'
+        toggleElement: '#online-biz-toggle',
+        fullDetails: '#ob-full-details'
     });
-    //onlineBizPanel.expand();
-
 });
