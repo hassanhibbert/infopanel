068e66b06dadaf875b1e155196ce79f0442fa2f4
Hassan Hibbert <hassanhibbert@gmail.com>
2016-04-06 18:03:01 -0400

    added refactored code. implemented different concept on handling data.

--- js/ajaxmanager.js
+++ js/ajaxmanager.js
@@ -2,62 +2,53 @@
 
 var ajaxManager = (function ($, document) {
 
-    var serviceUrl = 'js/fakeData_5.json',
+    var serviceUrl = 'js/fakeData_1000Plus.json',
+        
+        // cache vars
         cachedLength,
+        parsedData = [],
+        
+        // module API
         publicAPI,
-        parsedData = [];
-    
+        
+        // manage cached data vars
+        buildItems,
+        start = 0,
+        end = 0,
+        trackedLength = 0;
+
     //###############//
     //### Private ###//
     //###############//
-    
+
     /**
-     * Cache the data received from Ajax Request in [parsedData]
+     * Cache the data received from Ajax Request in [parsedData].
      *
-     * @param {object} object from JSON file
+     * @param {array} array from JSON file
      */
     function cacheData(data) {
-         data.forEach(function(obj){
-             parsedData.push(obj);
-         });
+        data.forEach(function (obj) {
+            parsedData.push(obj);
+        });
     }
-    
+
     /**
-     * Ajax call for data. Updates cached data, and data length for the [loadMore] method
+     * Ajax request for data. Receives data and caches it in [parsedData], as well as caching its length.
      *
-     * @param {function} callback to process the data received from ajax call
-     * @param {string} url of the new service call to retrieve more data for [loadMore] method
-     * @param {boolean} check to see if the method calling this function is [loadMore]
+     * @param {interger} how much data to build to DOM on initial load
+     * @param {function} callback function to process cached data.
      */
-    function getData(cb, newUrl, loadMore) {     
-        var buildItems,
-            sURL = (newUrl) ? newUrl : serviceUrl; // assign new service url
-        
+    function ajaxRequest(limit, cb) {
         $.ajax({
             method: 'GET',
-            url: sURL,
-            success: function (data) {
-                if (loadMore) {
-                    
-                    // new items to build
-                    buildItems = data.slice(cachedLength);
-                    
-                    // pass new items to callback. NOTE: will be inserted into the DOM at this time.
+            url: serviceUrl,
+            success: function successCB(data) {
+                cacheData(data);
+                cachedLength = parsedData.length;
+                buildItems = getBuildItems(limit);
+                if (typeof cb === 'function') {
                     cb(buildItems);
-                    
-                    // cache the new build items and update the current cached length
-                    cacheData(buildItems);
-                    cachedLength = parsedData.length;
-                    console.log(cachedLength);
-                } else {
-                    
-                    // cache build items and its length
-                    cacheData(data);
-                    cachedLength = parsedData.length;
-                    if (typeof cb === 'function') {
-                        cb(parsedData);
-                    }
-                }  
+                }
             },
             error: function () {
                 throw "Error loading data from url";
@@ -65,53 +56,79 @@ var ajaxManager = (function ($, document) {
         });
     }
     
+    /**
+     * Splits up the cached data in to smaller segments to return
+     *
+     * @param {interger} limit on how much to segment the data
+     * @return {array} returns a specific amount [itemLimit] of items from the cached data array.
+     */
+    function getBuildItems(itemLimit) {
+        var diff = end - start,
+            calcLimit = diff - itemLimit,
+            items;
+        end -= calcLimit;
+        items = parsedData.slice(start, end);
+        start += itemLimit;
+        end += itemLimit;
+        trackedLength += items.length;
+        return items;
+    }
+
     //##############//
     //### Public ###//
     //##############//
-      
+
     /**
-     * Search cached data to find the object that matches the provided key/value
+     * Search cached data to find the object that matches the provided key/value pair
      *
-     * @param {string} key name to look for
-     * @param {string} value name to look for
+     * @param {string} key string to look for
+     * @param {string} value string to look for
      * @return {object} returns the object with all the information associated with the provided key/value pair
      */
     function search(key, value) {
         var result;
-        parsedData.forEach(function(dataObj){  
+        parsedData.forEach(function (dataObj) {
             if (dataObj[key] === value) {
                 result = dataObj;
             }
         });
         return (result) ? result : 'No results found.';
     }
-    
+
     /**
-     * Load more data
+     * Load more items to DOM
      *
-     * @param {integer} how much data to request
+     * @param {integer} how much build items to request
      * @param {function} callback for processing data received
      */
     function loadMore(limit, cb) {
-        // TODO: configue limit
-        getData(cb, 'js/fakeData_10.json', true);
+        buildItems = getBuildItems(limit);
+        if (cachedLength === trackedLength) { 
+            return; 
+        }
+        if (typeof cb === 'function') {
+             cb(buildItems);
+        }
     }
     
     /**
      * Load initial data
      *
      * @param {function} callback for processing data received
+     * @param {interger} [optional] default number is 10 but can be changed of the  [limit] argument available 
      */
-    function loadData(cb) {
-        getData(cb);
+    function loadData(cb, limit) {
+        limit = (limit && typeof limit === 'number') ? limit : 10;
+        ajaxRequest(limit, cb);
     }
-    
+
     publicAPI = {
         loadData: loadData,
         search: search,
         loadMore: loadMore
     };
-
+    
+    // expose publicAPI methods
     return publicAPI;
 
 })(jQuery, document);
--- js/onlinebizpanel.js
+++ js/onlinebizpanel.js
@@ -15,7 +15,7 @@ var onlineBizPanel = (function ($, document) {
         
         shadowViewport = true,
         
-        // scroll position
+        // scroll position vars
         scrollPosition = {
             top: [],
             temp: null,
@@ -36,8 +36,7 @@ var onlineBizPanel = (function ($, document) {
     //###############//
     
     /**
-     * Toggles classes to animate the panel height
-     * and up/down arrow
+     * Toggles classes to animate the panel height and rotate chevron
      */
     function expandCollapsePanel() {
         $listContainter.toggleClass('ob-expanded ob-collapse');
@@ -51,41 +50,23 @@ var onlineBizPanel = (function ($, document) {
      * @param {boolean}
      * @param {element} Element to show
      */
-    function changeContent(update, $showEl) {   
+    function changeContent(update, $showEl) { 
+        // change elements and content if "update = true"
         var $hideEl = (update) ? $listInnerContainter : $fullDetails;
         
         $hideEl.addClass('ob-hide');
         $hideEl.toggleClass('ob-fadeOut ob-fadeIn');
         
         $showEl.removeClass('ob-hide');
-        
         $showEl.outerHeight(); // trigger css reflow/repaint
-        
         $showEl.toggleClass('ob-fadeOut ob-fadeIn');
-    
+        
         // set scroll top postion to the last known Y-Coords
         if (!update) {
             scrollPosition.set($listContainter[0]);
         } 
     }
     
-    /**
-     * Creates elements with attributes on-the-fly
-     *
-     * @param {string} The element to create. Example: div, a, img...
-     * @param {object} Object containing attribute name/value pairs
-     * @return {element} Newly created element with attribute name/value pairs
-     */
-    function myCreateElement(elementType, attributes) {
-        var el = document.createElement(elementType);
-        if (attributes) {
-            Object.keys(attributes).forEach(function (type) {
-                el.setAttribute(type, attributes[type]);
-            });
-        }
-        return el;
-    }
-
     /**
      * Handler for panel clicks
      * 
@@ -100,11 +81,11 @@ var onlineBizPanel = (function ($, document) {
      * 
      * @param {event} Click event information
      */
-    function handleBackClick(evt) {      
-        $backButton.addClass('ob-hide');
-        $('.bottom-wrap').remove();
-        $('.top-wrap').remove();        
+    function handleBackClick(evt) {
         changeContent(false, $listInnerContainter);
+        
+        $backButton.addClass('ob-hide');
+        $('.bottom-wrap, .top-wrap').remove();   
     }
     
     /**
@@ -143,16 +124,41 @@ var onlineBizPanel = (function ($, document) {
         
         // update with the current scroll position.
         scrollPosition.update(currentScrollTop);
+        
+        //console.log(evt);
+        //console.log(evt.target.scrollHeight);
+        if (currentScrollTop === (evt.target.scrollHeight - evt.target.clientHeight)) {
+            console.log('load more data!');
+            ajaxManager.loadMore(10, buildList);
+        }
+        
     }
     
     /**
      * Handler for mousedown. Main purpose of this handler is to store the
      * current scroll positon
      * 
-     * @param {event} click event
+     * @param {event} mouse down event
      */
     function handleMouseDown(evt) {
         scrollPosition.save();
+    }
+    
+    /**
+     * Creates elements with attributes on-the-fly
+     *
+     * @param {string} The element to create. Example: div, a, img...
+     * @param {object} Object containing attribute name/value pairs
+     * @return {element} Newly created element with attribute name/value pairs
+     */
+    function myCreateElement(elementType, attributes) {
+        var el = document.createElement(elementType);
+        if (attributes) {
+            Object.keys(attributes).forEach(function (type) {
+                el.setAttribute(type, attributes[type]);
+            });
+        }
+        return el;
     }
     
     /**
@@ -211,7 +217,7 @@ var onlineBizPanel = (function ($, document) {
      * Truncate string
      * 
      * @param {string} string to be truncated
-     * @param {integer} number limit on how much to display
+     * @param {integer} number limit on how much to display including the three dots
      * @return {string} returns truncated string
      */
     function truncate(str, num) {
@@ -221,8 +227,12 @@ var onlineBizPanel = (function ($, document) {
         return str;
     }
     
+    //##############//
+    //### Public ###//
+    //##############//
+    
     /**
-     * Builds out DOM element and append them to DOM
+     * Creates DOM elements with data from the argument [data]
      * 
      * @param {array} contains objects with data to be iterated over 
      */
@@ -232,7 +242,7 @@ var onlineBizPanel = (function ($, document) {
 
         data.forEach(function (dataObj) {
             // create elements
-            var liTag = myCreateElement('li'),
+            var liTag = myCreateElement('li', {class: 'ob-fadeOut'}),
                 linkDetails = myCreateElement('a', {class: 'ob-details', href: '#', 'data-businessname': dataObj.businessName}),
                 details = myCreateElement('p'),
                 phone = myCreateElement('div', {class: 'ob-phone'}),
@@ -243,7 +253,9 @@ var onlineBizPanel = (function ($, document) {
             // append data to elements
             $(liTag).append(linkDetails, phone, webLink);
             $(hTag).append(dataObj.businessName);
+            
             $(details).append(shortDetails);
+            
             $(linkDetails).append(hTag, details);
             $(phone).append(dataObj.phone);
             $(webLink).append(dataObj.website);
@@ -254,11 +266,12 @@ var onlineBizPanel = (function ($, document) {
 
         // append to DOM
         $listInnerContainter.append(docFrag);
+        
+        // TODO: temp example showing fade in when loading more data
+        setTimeout(function() {
+            $listInnerContainter.children('li').removeAttr('class');
+        }, 350);
     }
-
-    //##############//
-    //### Public ###//
-    //##############//
 
     /**
      * Collapse panel 
@@ -292,6 +305,8 @@ var onlineBizPanel = (function ($, document) {
         $backButton = $(opt.backButton);
 
         // get data and build list to DOM
+        // the default will build out 10 list items.
+        // to change it, pass a number argument. Example: ajaxManager.loadData(buildList, 30);
         ajaxManager.loadData(buildList);
 
         // listener for toggle element
@@ -304,23 +319,21 @@ var onlineBizPanel = (function ($, document) {
         $listContainter.on('scroll', handleDropShadow);
         
         // listener for back button
-        $backButton.on('click', handleBackClick);
+        $backButton.bind('click', handleBackClick);
         
+        // listener for mouse down: handler captures current scroll position
         $listInnerContainter.on('mousedown', handleMouseDown.bind(scrollPosition));
         
-        // DEBUG: temporary listener to test load more method
-        $('#loadbutton').bind('click', function(evt) {
-            evt.preventDefault();
-            ajaxManager.loadMore(5, buildList);
-        });
     }
     
     publicAPI = {
         init: init,
         expand: expand,
-        collapse: collapse
+        collapse: collapse,
+        buildList: buildList
     };
-
+    
+    // expose publicAPI methods
     return publicAPI;
     
 })(jQuery, document);
@@ -333,5 +346,11 @@ $(document).ready(function () {
         toggleElement: '#online-biz-toggle',
         fullDetails: '#ob-full-details',
         backButton: '#back-button'
+    });
+    
+    // DEBUG: temporary listener to test load more method
+    $('#loadbutton').bind('click', function(evt) {
+        evt.preventDefault();
+        ajaxManager.loadMore(10, onlineBizPanel.buildList);
     });
 });
